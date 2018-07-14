de4f42f3f2e0a526fa9fa1f4dbb0673fda8e901c
rachellefelzien <felzr88@gmail.com>
2018-07-14 12:37:58 -0700

    Generation script is working!!!

--- simple_grid_vertical_motion/generate_from_text.sh
+++ simple_grid_vertical_motion/generate_from_text.sh
@@ -1,46 +1,40 @@
 DOCUMENT_OPENING="<!DOCTYPE html>
 <html>
 
-<script>
-</script>
-
 <body>
 
-<link rel="stylesheet" type="text/css" href="20_wide_40_tall_css.css"> 
+<link rel=\"stylesheet\" type=\"text/css\" href=\"20_wide_40_tall_css.css\"> 
 
-<div class="divs_array">"
+<div class=\"divs_array\">"
 
 DOCUMENT_CLOSE="
 </div>
 
-<script src="wisp_flame_animation_bundle.js"></script> 
-
-</body>
-</html>
+<script src=\"wisp_flame_animation_bundle.js\"></script> 
 
 </body>
 </html>"
 
-DIV_ROW_OPEN="<div class="line">"
+DIV_ROW_OPEN="<div class=\"line\">"
 
 DIV_ROW_CLOSE="</div>"
 
-COLORED_DIV="<div class=square white></div>"
-BLANK_DIV="<div class=square blank></div>"
+COLORED_DIV="<div class=\"square white\"></div>"
+BLANK_DIV="<div class=\"square blank\"></div>"
+
+FILE_NAME="lacework_output.html"
 
 ####################################################
 
 make_div_row () {
 	#Create a row of divs from line
 	LINE=$1
-	#${parameter//pattern/string}
-	#echo $LINE | sed 'y/X/$COLORED_DIV/'
 	for (( i=0; i<${#LINE}; i++ )); do
 		CHAR="${LINE:$i:1}" 
 		if [ $CHAR == "X" ]; then
-			echo $COLORED_DIV
+			echo $COLORED_DIV >> $FILE_NAME
 		elif [ $CHAR == "O" ]; then
-			echo $BLANK_DIV
+			echo $BLANK_DIV >> $FILE_NAME
 		fi
 
 	done
@@ -48,26 +42,18 @@ make_div_row () {
 
 ####################################################
 
-touch lacework_output.html
+touch $FILE_NAME
 
 #Add the document open
-echo $DOCUMENT_OPENING >> lacework_output.html
-
-
-#Add the table by building up rows
-#for ((c=0; c<=$ARRAY_HEIGHT; c++ ))
-#do
-#	echo $DIV_ROW_OPEN >> lacework_output.html
-#	make_div_row
-#	echo $DIV_ROW_CLOSE >> lacework_output.html
-#done
+echo $DOCUMENT_OPENING >> $FILE_NAME
 
 file="./lacework_text.txt"
 while IFS= read -r line
 do
-	#echo "$line"
+	echo $DIV_ROW_OPEN >> $FILE_NAME
 	make_div_row $line
+	echo $DIV_ROW_CLOSE >> $FILE_NAME
 done <"$file"
 
 #Add the document close
-echo $DOCUMENT_CLOSE >> lacework_output.html
+echo $DOCUMENT_CLOSE >> $FILE_NAME
